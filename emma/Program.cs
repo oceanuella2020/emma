37ea4f5ab4ccf4ee1b664c1ac1a826d7922f4428
EMMANUELLA FATOLU <fatoluemmanuella@gmail.com>
2020-03-16 11:22:24 +0100

    Add project files.

---
+++ emma/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace emma
+{
+    class Program
+    {
+        private const string Value = "Hello world";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Value);
+            Console.WriteLine(Value);
+            Console.WriteLine(Value);
+            Console.WriteLine(Value);
+            Console.ReadKey();
+        }
+    }
+}
